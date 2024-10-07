21a1969c050ebaba621c8dddd44f19be122e2a36
sabbirhassanict <sabbirmurad.doict@gmail.com>
2024-10-08 04:45:05 +0600

    Server running successfully

--- server.js
+++ server.js
@@ -30,3 +30,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send(
+    "Hello From devAlienBrain's Tasty Tales Recipe Sharing Platform SERVER!"
+  );
+});
